7c1d57372eb886c627f1da6a32237cc539155cf8
Pasha <oxeygenoxeygen@gmail.com>
2017-05-10 14:46:39 +0300

    fix properties

--- src/main/java/com/netcracker/crm/controller/rest/GroupRestController.java
+++ src/main/java/com/netcracker/crm/controller/rest/GroupRestController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
